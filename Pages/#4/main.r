97c11077acf2e133de2e627000db23806120d194
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-06-08 15:35:58 +0200

    Create main.r

---
+++ Pages/#4/main.r
@@ -0,0 +1,19 @@
+# == Equal
+# != Not equal
+# > Greater than
+# < Smaller than
+# >= Greater than or equal
+# <= Smaller than or equal
+
+a <- 5
+b <- 7
+
+if (a == b) {
+  "a == b"
+} else if (a == 5) {
+  "a == 5"
+} else if (a == 5) {
+  "a == 5 (2)"
+} else {
+  "a != b"
+}
